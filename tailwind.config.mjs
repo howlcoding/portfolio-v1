80f91257bbcb24e8ba9b3563c21a55b8eda96665
Willow Laing <willow.laing12@gmail.com>
2024-05-05 18:51:23 -0400

    changed from nextui to flowbite, updated image and title + tagline content

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,18 @@
-// tailwind.config.mjs
-import { nextui } from "@nextui-org/react";
-
 /** @type {import('tailwindcss').Config} */
-export const content = [
-	// ...
-	"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-];
-export const theme = {
-	extend: {},
-};
-export const darkMode = "class";
-export const plugins = [nextui()];
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/flowbite/**/*.js'],
+	theme: {
+		extend: {},
+		colors: {
+			babyblue: '#22C3F1',
+			primaryblue: '#272949',
+			limegreen: '#4AC06F',
+			darkpurple: '#9251F7',
+			darkblue: '#516CF7',
+			graybg: '#f3f4f6',
+		  },
+	},
+	plugins: [
+		require('flowbite/plugin')
+	],
+}
